8f9754025cc074e309f57ac425c4a576f1dc8d4d
Piotr Myszkowski <Piotr.Myszkowski@emerson.com>
2021-04-24 14:43:08 +0200

    Implementation of generic domain building blocks

---
+++ src/Acme.LoanCalculator.Core/Domain/Generic/AnnualInterestRate.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Acme.LoanCalculator.Core.Domain.Generic
+{
+    public sealed class AnnualInterestRate : IEquatable<AnnualInterestRate>
+    {
+        public AnnualInterestRate(PercentRate annualRate)
+        {
+            AnnualRate = annualRate;
+        }
+
+        public PercentRate AnnualRate { get; }
+
+        public PercentRate MonthlyRate => AnnualRate / 12;
+
+        public static bool operator ==(AnnualInterestRate left, AnnualInterestRate right)
+        {
+            return Equals(left, right);
+        }
+
+        public static bool operator !=(AnnualInterestRate left, AnnualInterestRate right)
+        {
+            return !Equals(left, right);
+        }
+
+        public bool Equals(AnnualInterestRate other)
+        {
+            if (ReferenceEquals(null, other)) return false;
+            if (ReferenceEquals(this, other)) return true;
+            return Equals(AnnualRate, other.AnnualRate);
+        }
+
+        public override bool Equals(object obj)
+        {
+            return ReferenceEquals(this, obj) || obj is AnnualInterestRate other && Equals(other);
+        }
+
+        public override int GetHashCode()
+        {
+            return (AnnualRate != null ? AnnualRate.GetHashCode() : 0);
+        }
+    }
+}
---
+++ src/Acme.LoanCalculator.Core/Domain/Generic/Money.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Acme.LoanCalculator.Core.Domain.Generic
+{
+    public sealed class Money : IEquatable<Money>, IComparable<Money>, IComparable
+    {
+        public Money(decimal amount, Currency currency)
+        {
+            Amount = amount;
+            Currency = currency ?? throw new ArgumentNullException(nameof(currency));
+        }
+
+        public static Money DanishCrones(decimal amount) => new Money(amount, Currency.DanishCrone);
+
+        public decimal Amount { get; }
+
+        public Currency Currency { get; }
+
+        public static Money operator +(Money left, Money right)
+        {
+            AssertIsCurrencyTheSame(left, right);
+            return new Money(left.Amount + right.Amount, left.Currency);
+        }
+
+        public static Money operator -(Money left, Money right)
+        {
+            AssertIsCurrencyTheSame(left, right);
+            return new Money(left.Amount - right.Amount, left.Currency);
+        }
+
+        public static bool operator ==(Money left, Money right)
+        {
+            return Equals(left, right);
+        }
+
+        public static bool operator !=(Money left, Money right)
+        {
+            return !Equals(left, right);
+        }
+
+        public static bool operator <(Money left, Money right)
+        {
+            return Comparer<Money>.Default.Compare(left, right) < 0;
+        }
+
+        public static bool operator >(Money left, Money right)
+        {
+            return Comparer<Money>.Default.Compare(left, right) > 0;
+        }
+
+        public static bool operator <=(Money left, Money right)
+        {
+            return Comparer<Money>.Default.Compare(left, right) <= 0;
+        }
+
+        public static bool operator >=(Money left, Money right)
+        {
+            return Comparer<Money>.Default.Compare(left, right) >= 0;
+        }
+
+        public static void AssertIsCurrencyTheSame(Money left, Money right)
+        {
+            if (left == null) throw new ArgumentNullException(nameof(left));
+            if (right == null) throw new ArgumentNullException(nameof(right));
+            if (left.Currency != right.Currency) throw new InvalidOperationException("Money operands must be of the same type.");
+        }
+
+        public override string ToString()
+        {
+            return $"{Amount:C} {Currency}";
+        }
+
+        public bool Equals(Money other)
+        {
+            if (ReferenceEquals(null, other)) return false;
+            if (ReferenceEquals(this, other)) return true;
+            return Amount == other.Amount && Equals(Currency, other.Currency);
+        }
+
+        public override bool Equals(object obj)
+        {
+            return ReferenceEquals(this, obj) || obj is Money other && Equals(other);
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Amount, Currency);
+        }
+
+        public int CompareTo(Money other)
+        {
+            AssertIsCurrencyTheSame(this, other);
+            if (ReferenceEquals(this, other)) return 0;
+            if (ReferenceEquals(null, other)) return 1;
+            return Amount.CompareTo(other.Amount);
+        }
+
+        public int CompareTo(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return 1;
+            if (ReferenceEquals(this, obj)) return 0;
+            return obj is Money other ? CompareTo(other) : throw new ArgumentException($"Object must be of type {nameof(Money)}");
+        }
+    }
+}
