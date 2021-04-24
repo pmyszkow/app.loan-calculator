b8fbe857097a4a6d26ca44945f0d0f9deb9fa77a
Piotr Myszkowski <Piotr.Myszkowski@emerson.com>
2021-04-24 20:10:30 +0200

    Policies added

---
+++ src/Acme.LoanCalculator.Core/Domain/Core/DefaultCommissionPolicy.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Acme.LoanCalculator.Core.Domain.Generic;
+
+namespace Acme.LoanCalculator.Core.Domain.Core
+{
+    public class DefaultCommissionPolicy : ICommissionPolicy
+    {
+        private readonly PercentRate COMMISION_RATE = new PercentRate(1);
+        private readonly Money MAX_COMMISSION = Money.DanishCrones(10000);
+
+        public Money Calculate(Money loanAmount)
+        {
+            Money.AssertIsCurrencyTheSame(loanAmount, MAX_COMMISSION);
+
+            if (loanAmount == null) throw new ArgumentNullException(nameof(loanAmount));
+
+            var commission = loanAmount * COMMISION_RATE.DecimalRate;
+
+            return commission > MAX_COMMISSION ? MAX_COMMISSION : commission;
+        }
+    }
+}
---
+++ src/Acme.LoanCalculator.Core/Domain/Core/IAopPolicy.cs
@@ -0,0 +1,9 @@
+﻿using Acme.LoanCalculator.Core.Domain.Generic;
+
+namespace Acme.LoanCalculator.Core.Domain.Core
+{
+    public interface IAopPolicy
+    {
+        PercentRate Calculate(Money amount, Money totalInterest, Money commission, MonthsDuration duration);
+    }
+}
