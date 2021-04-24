9e355f60af9cbc93c66e6321223e21ebe5af6ae4
Piotr Myszkowski <Piotr.Myszkowski@emerson.com>
2021-04-24 18:50:38 +0200

    Annuity VO added

--- src/Acme.LoanCalculator.Core/Domain/Core/Annuity.cs
+++ src/Acme.LoanCalculator.Core/Domain/Core/Annuity.cs
@@ -1,7 +1,49 @@
-﻿namespace Acme.LoanCalculator.Core.Domain.Core
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Acme.LoanCalculator.Core.Domain.Generic;
+
+namespace Acme.LoanCalculator.Core.Domain.Core
 {
-    public class Annuity
+    public sealed class Annuity : IEquatable<Annuity>
     {
-        
+        public Annuity(IReadOnlyList<Payment> payments)
+        {
+            PaymentsList = payments ?? throw new ArgumentNullException(nameof(payments));
+        }
+
+        public IReadOnlyList<Payment> PaymentsList { get; }
+
+        public Money GetTotalInterest()
+        {
+            return PaymentsList.Aggregate(Money.Zero, (current, payment) => current + payment.Interest);
+        }
+
+        public static bool operator ==(Annuity left, Annuity right)
+        {
+            return Equals(left, right);
+        }
+
+        public static bool operator !=(Annuity left, Annuity right)
+        {
+            return !Equals(left, right);
+        }
+
+        public bool Equals(Annuity other)
+        {
+            if (ReferenceEquals(null, other)) return false;
+            if (ReferenceEquals(this, other)) return true;
+            return other.PaymentsList.SequenceEqual(PaymentsList);
+        }
+
+        public override bool Equals(object obj)
+        {
+            return ReferenceEquals(this, obj) || obj is Annuity other && Equals(other);
+        }
+
+        public override int GetHashCode()
+        {
+            return (PaymentsList != null ? PaymentsList.GetHashCode() : 0);
+        }
     }
 }
