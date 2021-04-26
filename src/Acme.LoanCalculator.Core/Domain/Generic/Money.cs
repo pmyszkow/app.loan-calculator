f45faee8f7f14a5cbe573f32c2091b13fbcfb58b
Piotr Myszkowski <Piotr.Myszkowski@emerson.com>
2021-04-26 10:25:26 +0200

    Money refactor

--- src/Acme.LoanCalculator.Core/Domain/Generic/Money.cs
+++ src/Acme.LoanCalculator.Core/Domain/Generic/Money.cs
@@ -13,7 +13,7 @@ namespace Acme.LoanCalculator.Core.Domain.Generic
 
         public static Money FromDanishCrones(decimal amount) => new Money(amount, Currency.DanishCrone);
 
-        public static Money Zero { get; } = new Money(Decimal.Zero, Generic.Currency.Default);
+        public static Money Zero { get; } = new Money(0m, Generic.Currency.Default);
 
         public decimal Amount { get; }
 
